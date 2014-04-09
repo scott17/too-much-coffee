2dddc5d930f45a691c6e96c753c41e83cf742c96
Andrew Gallagher <ndrwg@me.com>
2014-04-09 11:51:13 -0400

    Add package def and required imports

--- too-much-coffee.lisp
+++ too-much-coffee.lisp
@@ -1,3 +1,24 @@
 ;;this is our main lisp file
 
 ;; this is an edit
+
+
+;; Package Definition
+(defpackage :too-much-coffee)
+(in-package :too-much-coffee)
+
+
+;; Imports
+(import '(user::BeanTypes 
+           user::BeanConversion
+           user::game-deck-stats user::game-discards user::game-discard-stats 
+           user::game-coin-stats user::game-players user::game-rounds 
+           user::game-shuffles
+           user::player-name user::player-hand user::player-faceup
+           user::player-numfields user::player-fields user::player-coins
+           user::player-coin-stack
+           user::plant user::harvest user::bean-fits
+           user::legal-fields-to-harvest user::plant-card-at-front
+           user::harvest-rate user::buy-third-bean-field
+           user::is-singleton? user::is-multiple? user::is-empty?
+           user::is-planted?))
