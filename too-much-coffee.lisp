16b62b02a326aa6a740664c5a5eb28fdaf34e1aa
Andrew Gallagher <ndrwg@me.com>
2014-04-13 15:57:56 -0400

    Comment value

--- too-much-coffee.lisp
+++ too-much-coffee.lisp
@@ -42,31 +42,48 @@
 (defun choose-harvest (player game)
   )
 
+; Value to force a useless bean to a very small heuristic value.
+; Avoids divide-by-zero errors if we hold enough beans to
+; eliminate the possibility of a higher coin tier.
 (defconstant *USELESS-BEAN* 1000000)
 
 (defun value (player card game)
-  (let* ((cards-in-play (+ (cdr (assoc card) BeanTypes)
-                           (- (cdr (assoc card (game-coin-stats game))))
-                           (- (cdr (assoc card (game-discard-stats game))))
-                           (- 1)))
+  "Calculates the value of a card using our heuristic"
+  (let*
+    ; Calculate the number of cards of this type that are not
+    ; in a field or held in a player's hand.
+    ((cards-in-play (+ (cdr (assoc card) BeanTypes)
+                       (- (cdr (assoc card (game-coin-stats game))))
+                       (- (cdr (assoc card (game-discard-stats game))))
+                       (- 1)))
 
-         (held-field (assoc card (player-fields player)))
-         (conversion (assoc card BeanConversion))
+     ; If we have a field containing this card type, this stores the field.
+     ; Otherwise, nil (length of nil is 0).
+     (held-field (assoc card (player-fields player)))
+     ; Card to coin conversion for this bean.
+     (conversion (assoc card BeanConversion))
 
-         (beans-held (length held-field))
-         (beans-to-next-tier (cond
-                               ((and (second convesion) (< (second conversion) beans-held))
-                                (- (second conversion) beans-held))
-                               ((and (third conversion) (< (third conversion) beans-held))
-                                (- (third conversion) beans-held))
-                               ((and (fourth conversion) (< (fourth conversion) beans-held))
-                                (- (fourth conversion) beans-held))
-                               ((and (fifth conversion) (< (fifth conversion) beans-held))
-                                (- (fifth conversion) beans-held))
-                               (t *USELESS-BEAN*)))
+     ; Number of beans held (0 if held-field is nil, meaning no field has this type).
+     (beans-held (length held-field))
 
-         (coins-earned (harvest-rate held-field)))
+     ; Number of beans needed to get to the next tier of coins. If there
+     ; is no higher tier of coins, *USELESS-BEAN* to avoid divide-by-zero
+     ; and make the bean useless relative to others (harvested early).
+     (beans-to-next-tier (cond
+                           ((and (second convesion) (< (second conversion) beans-held))
+                            (- (second conversion) beans-held))
+                           ((and (third conversion) (< (third conversion) beans-held))
+                            (- (third conversion) beans-held))
+                           ((and (fourth conversion) (< (fourth conversion) beans-held))
+                            (- (fourth conversion) beans-held))
+                           ((and (fifth conversion) (< (fifth conversion) beans-held))
+                            (- (fifth conversion) beans-held))
+                           (t *USELESS-BEAN*)))
 
+     ; Number of coins earned if we harvest the field.
+     (coins-earned (harvest-rate held-field)))
+
+    ; Heuristic calcuation.
     (* (/ cards-in-play beans-to-next-tier) coins-earned)))
 
 (defun at-risk? (player card game)
