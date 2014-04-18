38bbdfdae2ab20d1c755b8c008e94ecb1f377861
Scott Bryant <scott17@umbc.edu>
2014-04-18 08:53:37 -0400

    Handle-face-up-cards: Commented and removed elses
    Still not tested.

--- too-much-coffee.lisp
+++ too-much-coffee.lisp
@@ -81,15 +81,18 @@
 
 
 (defun handle-face-up-cards (player game)
-    ;guarenteed to work
-    ;stupid
-    ;will be changed later before the final submit
+    ;These first two are guarenteed to work
+    ;They are stupid
+    ;This will be changed later before the final submit
     ;(plant-card player (pop (player-faceup player)) game)
     ;(plant-card player (pop (player-faceup player)) game)
     
+    
     (progn 
+           ; First get the two cards 
            (setq first-card (pop (player-faceup player)))
            (setq second-card (pop (player-faceup player)))
+           ; Plant the cards in matching fields if they exist
            (let ((same-field (assoc first-card (player-fields player))))
                       (if (not (equal same-field nil))
                        (progn
@@ -100,23 +103,32 @@
                        (progn
                                  (plant-card player second-card)
                                   (setq second-card nil))))
+           ; If we planted both of the cards we are done
            (if (and (equal first-card nil) (equal second-card nil))
                       (return-from handle-face-up-cards))
+           ; If both aren't planted yet
            (if (and (not (equal first-card nil)) (not (equal (second-card nil))))
+                      ; If the first is more valuable than the second,
+                      ;   plant the second card first and then the first card.
+                      ;   You want the most valuable card planted last to ensure that
+                      ;   it will be in the fields
                       (if (> (value player first-card game) (value player second-card game))
                                  (progn
                                             (plant-card player second-card)
                                             (plant-card player first-card)
-                                            (return-from handle-face-up-cards)))
-                      (else
+                                            (return-from handle-face-up-cards))
+                      ;else
+                      ; Plant the first card first and the second last
                                  (progn
                                             (plant-card player first-card)
                                             (plant-card player second-card)
                                             (return-from handle-face-up-cards))))
+           ; Finally, if the first isn't planted yet,
+           ;  plant it.
            (if (not (equal first-card nil))
-                      (plant-card player first-card))
-           (else
-                      (plant-card player second-card)))
+                      (plant-card player first-card)
+           ; Else, plant the second card
+                      (plant-card player second-card))
                                  
   )
 
